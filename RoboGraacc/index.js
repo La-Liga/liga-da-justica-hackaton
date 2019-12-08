e8bf5aec8843ca1d1e3d1d603be212d821899b8d
Wescley Matos <walvesm@indracompany.com>
2019-12-07 22:27:15 -0300

    #11 - Refatoração do robo

--- RoboGraacc/index.js
+++ RoboGraacc/index.js
@@ -1,36 +1,9 @@
-// const mongoose = require('mongoose');
-// const credentials = require('./config/credentials.json').MongoBD;
-// //const Publicacao = require('./models/publicacao');
+const robots = {
+    tjes: require('./robots/tjes.js'),
+}
 
-// const pass = credentials.pass;
-// const user = credentials.user;
-// const connectionString = `mongodb://${user}:${pass}@ds253348.mlab.com:53348/hackaton_graacc`;
+function start() {
+    robots.tjes();
+}
 
-// mongoose.connect(connectionString, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-// const db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//     console.log('Connected');
-//});
-
-// const publicacao = new Publicacao({
-//     titulo: 'titulo',
-//     link: 'link',
-//     resumo: 'resumo',
-//     texto: 'texto',
-//     dataDisponibilizacao: new Date
-// });
-
-// console.log(publicacao);
-
-// publicacao.save(function (err, publicacao) {
-//     if (err) return console.error(err);
-// });
-
-
-
+start();
